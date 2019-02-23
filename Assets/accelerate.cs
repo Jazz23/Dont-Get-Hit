44f260ff8b59eba4bdc8e36fe3da8a870cf2e6d1
Iizerd <jameswilliammcgowan@gmail.com>
2019-02-22 22:54:35 -0800

    more scripts
    translation, acceleration completed with stamina

--- Assets/accelerate.cs
+++ Assets/accelerate.cs
@@ -6,11 +6,12 @@ public class accelerate : MonoBehaviour
 {
     public float fl_maxspeed = 50F;
     public float fl_minspeed = -10F;
-    public float fl_acceleration = 5F;
-    public float fl_deceleration = 10F;
+    public float fl_acceleration = 0.1F;
+    public float fl_deceleration = 0.5F;
     public float fl_currentspeed = 0F;
-
-
+    public float fl_stamina = 100F;
+    public bool can_use_stamina = true;
+    
 	void Start()
     {
 		
@@ -18,11 +19,25 @@ public class accelerate : MonoBehaviour
 
 	void Update()
     {
-		if (Input.GetKey(KeyCode.W))
-            fl_currentspeed += fl_acceleration;
+        bool using_stamina = Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && fl_stamina > 0F && can_use_stamina;
+        if (using_stamina)  fl_stamina -= 0.5F;
+        else fl_stamina += 0.1F;
+
+        //disable stamina after used to 0 until it gets back to 20%
+        if (fl_stamina <= 0F) can_use_stamina = false;
+        else if (fl_stamina >= 20F)  can_use_stamina = true;
+
+        if (fl_stamina > 100) fl_stamina = 100;
+        else if (fl_stamina < 0) fl_stamina = 0;
+
+        if (Input.GetKey(KeyCode.W))
+            fl_currentspeed += fl_acceleration + (using_stamina ? fl_acceleration/2F : 0);
         else if (Input.GetKey(KeyCode.S))
             fl_currentspeed -= fl_deceleration;
+        
+        //Mathf.Clamp(fl_currentspeed, fl_minspeed, fl_maxspeed); uhh wtf why doesnt this work?
+        if (fl_currentspeed > fl_maxspeed)  fl_currentspeed = fl_maxspeed;
+        else if (fl_currentspeed < fl_minspeed)  fl_currentspeed = fl_minspeed;
 
-        Mathf.Clamp(fl_currentspeed, fl_minspeed, fl_maxspeed);
-	}
+    }
 }
---
+++ Assets/translate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translate : MonoBehaviour {
+
+    public float fl_translatespeed = 0.5F;
+
+	void Start()
+    {
+		
+	}
+
+	void Update()
+    {
+		if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * fl_translatespeed);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.left * -fl_translatespeed);
+	}
+}
