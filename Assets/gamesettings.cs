22a1c2b26101cefbf1d55d26dc538fa96e920423
Iizerd <47404005+Iizerd@users.noreply.github.com>
2019-02-25 15:40:10 -0800

    added stuff
    memeattack

--- Assets/gamesettings.cs
+++ Assets/gamesettings.cs
@@ -10,13 +10,16 @@ namespace Assets
 
         public static int i_health;
         
+        public static float fl_maxVel;
+        public static float fl_minVel;
         public static float fl_stamina;
+        public static float fl_speed;
         public static float fl_velocity;
         public static float fl_acceleration;
         public static float fl_yaw;
         public static float fl_pitch;
         public static float fl_friction;
-
+        
         public static bool b_useStamina;
     }
     public static class GameSettings
